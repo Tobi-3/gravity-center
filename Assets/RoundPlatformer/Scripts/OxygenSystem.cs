499367d6bfc5fce121669da6593b9fc1bc7c1403
Katharina Poppei <katharina.poppei@student.uni-tuebingen.de>
2023-01-06 12:33:54 +0100

    adds script for oxygen system

---
+++ Assets/RoundPlatformer/Scripts/OxygenSystem.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class OxygenSystem : MonoBehaviour
+{
+    [Header("Oxygen")]
+    [SerializeField] private float _maxOxygen = 100f;
+    [SerializeField] private float _oxygenDepletionRate = 1f;
+    private float _currentOxygen;
+    public float oxygenPercentage => _currentOxygen / _maxOxygen;
+
+    public static UnityAction OnPlayerDied;
+
+    private void Start() {
+        _currentOxygen = _maxOxygen;
+    }
+
+    private void Update() {
+        _currentOxygen -= _oxygenDepletionRate * Time.deltaTime;
+
+        if(_currentOxygen <= 0) {
+            OnPlayerDied?.Invoke();
+            _currentOxygen = 0f;
+        }
+    }
+
+    public void ReplenishOxygen(float oxygenAmount){
+        _currentOxygen += oxygenAmount;
+
+    if  (_currentOxygen > _maxOxygen)  _currentOxygen = _maxOxygen;
+
+
+    }
+
+}
+
